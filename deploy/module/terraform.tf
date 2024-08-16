fbbfc1887a77514a830727df0261fe4f0d1ec63d
t-young31 <tom.young.316@gmail.com>
2024-08-16 17:45:43 +0100

    add min version

--- deploy/module/terraform.tf
+++ deploy/module/terraform.tf
@@ -2,10 +2,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
+      version = ">=5.51.1"
     }
 
     kubernetes = {
       source  = "hashicorp/kubernetes"
+      version = ">=2.30.0"
     }
   }
 }
